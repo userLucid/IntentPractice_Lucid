1a503f46092b3d87094c256c136334e62a1b7726
windows <nvpz1598@gmail.com>
2021-07-22 11:43:41 +0900

    Intent 이용 -> 다른 화면으로 이동

--- app/src/main/java/com/example/intentpractice_lucid/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_lucid/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.intentpractice_lucid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+//            다른 화면으로 이동 (OtherActivity)
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+
+            startActivity(myIntent)
+
+
+        }
     }
 }
