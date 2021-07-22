3faf00fa5b8a6beae3e37d861fe56b39e7aaba7b
windows <nvpz1598@gmail.com>
2021-07-22 12:28:39 +0900

    전달해준 문구를 메세지 화면해서 사용

--- app/src/main/java/com/example/intentpractice_lucid/MessageActivity.kt
+++ app/src/main/java/com/example/intentpractice_lucid/MessageActivity.kt
@@ -2,10 +2,21 @@ package com.example.intentpractice_lucid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+//        이 화면에 들어올 때 첨부된 데이터를 텍스트뷰에 반영(올 때 뭐 없니?)
+//        받을 때 첨부한 이름표를 가지고 받기
+//        티켓 좀 내나봐
+
+        val intentMessage = intent.getStringExtra("message")
+
+        receiveMessegeTxt.text = intentMessage
+
+
     }
 }
