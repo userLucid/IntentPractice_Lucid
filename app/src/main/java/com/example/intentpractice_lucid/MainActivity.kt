911e7892fd9af2c24e11c5e7d2161166042ed27b
windows <nvpz1598@gmail.com>
2021-07-22 12:20:18 +0900

    입력한 문구를 데이터 첨부

--- app/src/main/java/com/example/intentpractice_lucid/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_lucid/MainActivity.kt
@@ -10,6 +10,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        sendMessageBtn.setOnClickListener {
+
+//          입력한 내용을 변수에 저장 get, set
+            val inputMessage = messageEdt.text.toString()
+
+
+            val myIntent = Intent(this, MessageActivity::class.java) //티켓
+
+            myIntent.putExtra("message", inputMessage) //수하물 첨부(갈 때 가지고 가)
+
+            startActivity(myIntent) //비행기
+
+
+        }
+
+
+
+
+
+
         moveToOtherBtn.setOnClickListener {
 
 //            다른 화면으로 이동 (OtherActivity)
